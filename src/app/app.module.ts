5b986b1e1abe3acdd1e2cc4056ff1568fdf08e6a
Nayara Pimentel <n.pimentel@vallourec.com>
2020-04-09 20:09:53 -0300

    Pratica Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EventosComponent } from './eventos/eventos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventosComponent
   ],
   imports: [
     BrowserModule
